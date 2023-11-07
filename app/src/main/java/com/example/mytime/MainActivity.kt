5612dffbdff2e723a5ab16860b355fa01cae1503
aghmnl <aghmnl@gmail.com>
2023-11-07 11:56:21 -0300

    Comments added by IA

--- app/src/main/java/com/example/mytime/MainActivity.kt
+++ app/src/main/java/com/example/mytime/MainActivity.kt
@@ -10,30 +10,35 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    // Declare variables for the chronometer and buttons
     private lateinit var chronometer: Chronometer
     private lateinit var startPauseButton: ImageButton
     private lateinit var resetButton: ImageButton
 
+    // Declare and initialize variables for the start state and elapsed time
     private var start = true
     private var elapsedTime = 0L
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Initialize the chronometer and buttons
         chronometer = findViewById(R.id.chronometer)
         startPauseButton = findViewById(R.id.startPauseButton)
         resetButton = findViewById(R.id.resetButton)
 
+        // Set an onClickListener for the start/pause button
         startPauseButton.setOnClickListener {
             if (start) {
+                // If the start variable is true, start the chronometer
                 chronometer.base = SystemClock.elapsedRealtime() - elapsedTime
                 chronometer.start()
                 start = false
                 startPauseButton.setImageResource(R.drawable.baseline_pause_24)
                 startPauseButton.contentDescription = getString(R.string.stop)
             } else {
+                // If the start variable is false, stop the chronometer and save the elapsed time
                 elapsedTime = SystemClock.elapsedRealtime() - chronometer.base
                 chronometer.stop()
                 start = true
@@ -42,7 +47,9 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        // Set an onClickListener for the reset button
         resetButton.setOnClickListener {
+            // Reset the chronometer and the start state
             chronometer.base = SystemClock.elapsedRealtime()
             chronometer.stop()
             elapsedTime = 0L
@@ -51,13 +58,16 @@ class MainActivity : AppCompatActivity() {
             startPauseButton.contentDescription = getString(R.string.start)
         }
 
+        // Initialize the progress bar
         val progressBar = findViewById<ProgressBar>(R.id.progressBar)
 
+        // Set an onChronometerTickListener for the chronometer
         chronometer.setOnChronometerTickListener {
             val elapsedMillis = SystemClock.elapsedRealtime() - chronometer.base
             val progress = (elapsedMillis / 1000).toInt() // Convert milliseconds to seconds
             progressBar.progress = progress
 
+            // If the progress bar is full, stop the chronometer and reset the start state
             if (progressBar.progress >= progressBar.max) {
                 Toast.makeText(this, "Time's up!", Toast.LENGTH_SHORT).show()
                 chronometer.stop()
@@ -65,7 +75,6 @@ class MainActivity : AppCompatActivity() {
                 start = true
                 startPauseButton.setImageResource(R.drawable.baseline_play_arrow_24)
                 startPauseButton.contentDescription = getString(R.string.start)
-
             }
         }
     }
