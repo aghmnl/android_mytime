a8f42e855405e2008c9b81bbe6c15b8037e47646
aghmnl <aghmnl@gmail.com>
2024-01-29 19:50:44 -0300

    Clock in its tab working

--- app/src/main/java/com/followapp/mytime/FirstFragment.kt
+++ app/src/main/java/com/followapp/mytime/FirstFragment.kt
@@ -1,6 +1,43 @@
 package com.followapp.mytime
 
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+//import android.view.LayoutInflater
+import android.view.View
+//import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
-class FirstFragment : Fragment(R.layout.fragment_first)
+class FirstFragment : Fragment(R.layout.fragment_clock) {
 // This takes the layout fragment_first and inflates it
+
+    private lateinit var textView: TextView
+    private val handler = Handler(Looper.getMainLooper())
+    private val runnable = object : Runnable {
+        override fun run() {
+            textView.text = SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(Date())
+            handler.postDelayed(this, 1000)
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        textView = view.findViewById(R.id.clock)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        handler.post(runnable)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        handler.removeCallbacks(runnable)
+    }
+
+
+}
